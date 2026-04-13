b37bdb5c3f7b2e52b3c3c02af9f252f195ec7171
guibenat <guibenattedev@gmail.com>
2026-04-12 22:57:00 -0300

    Criando a clase de atributos de alunos

---
+++ Matricula/src/Aluno.java
@@ -0,0 +1,22 @@
+public class Aluno {
+    private String nome;
+    private int numeroMatricula;
+    private String curso;
+
+    public Aluno(String nome, int numeroMatricula, String curso){
+        this.nome = nome;
+        this.numeroMatricula = numeroMatricula;
+        this.curso = curso;
+    }
+
+    public String getNome (){return  nome;}
+    public void setNome (String nome){this.nome = nome ;}
+
+    public int getNumeroMatricula () {return numeroMatricula;}
+    public void setNumeroMatricula (int numeroMatricula){this.numeroMatricula = numeroMatricula;}
+
+    public String getCurso () {return curso;}
+    public void setCurso (String curso) {this.curso = curso;}
+
+
+}
